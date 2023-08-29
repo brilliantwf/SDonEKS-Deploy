4af0d8fe38961572d4d57556372acf5657ee4f7d
brilliantwf <wf0504@qq.com>
2023-08-29 00:58:20 +0000

    new commit

--- init-env.sh
+++ init-env.sh
@@ -7,8 +7,6 @@ nvm install 16
 npm install -g aws-cdk
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.27.1/2023-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
-echo 'export PATH=$HOME/bin:$PATH' >> ~/.bash_profile
-source ~/.bash_profile
+sudo cp ./kubectl /usr/local/bin
 echo "Use bash shell via Terminal"
 
