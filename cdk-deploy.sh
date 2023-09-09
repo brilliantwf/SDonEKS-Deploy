5690dcd978447c6466f38faeb97270c9cce139ab
brilliantwf <wf0504@qq.com>
2023-09-09 06:30:04 +0000

    new commit

--- cdk-deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-git clone --recurse-submodules https://PRIVATE_TOKEN@github.com/bnusunny/stable-diffusion-on-eks.git
-cd ~/SageMaker/SDonEKS-Deploy/stable-diffusion-on-eks
-npm install
-cdk bootstrap
-cdk deploy --all —no-rollback
